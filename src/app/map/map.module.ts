dc9352e46d86d158eea6969a25f902aa0ec93152
maeda <takanori@stroly.jp>
2019-10-21 11:33:09 +0900

    angular material導入

--- src/app/map/map.module.ts
+++ src/app/map/map.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { CustomMaterialModule } from '../../core/custom-material.module';
 import { MapContainerComponent } from './map.container';
 import { ConsoleComponent } from './console/console.component';
 
 @NgModule({
   declarations: [MapContainerComponent, ConsoleComponent],
-  imports: [ FormsModule, ReactiveFormsModule],
+  imports: [CustomMaterialModule, FormsModule, ReactiveFormsModule],
   exports: [MapContainerComponent],
 })
-export class MapModule {}
+export class MapModule { }
